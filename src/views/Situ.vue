ac261cea30c4f3821415aa79dc0ab7ef570216d4
e <lt15613779885@163.com>
2020-12-14 15:17:00 +0800

    用vant排了个遮罩层"

--- src/views/Situ.vue
+++ src/views/Situ.vue
@@ -2,14 +2,8 @@
   <div class="situ">
     <header>
       <mt-header title="现场机械管理系统">
-        <router-link
-          to=""
-          slot="left"
-        >
-          <i
-            class="el-icon-arrow-left"
-            @click="back"
-          ></i>
+        <router-link to="" slot="left">
+          <i class="el-icon-arrow-left" @click="back"></i>
         </router-link>
         <mt-button slot="right">
           <i class="el-icon-bell"></i>
@@ -17,22 +11,43 @@
       </mt-header>
     </header>
     <main>
-      <mt-button
-        type="primary"
-        @click="add"
-      >+</mt-button>
-<<<<<<< HEAD
-      <mt-button
-        type="primary"
-        @click="zhLt"
-      >张浩李涛</mt-button>
-=======
-      <button
-        type="primary"
-        class="dzzy"
-        @click='dzzy'
-      >吊装作业</button>
->>>>>>> 20a47a4d195cc3673b1331a32a1594cbb93cad05
+      <mt-button type="primary" @click="add">+</mt-button>
+      <!-- 弹出层 -->
+      <van-button is-link @click="zhLt">日常检查</van-button>
+      <van-popup v-model="show" position="right" :style="{ height: '100%' }">
+        <div class="state">
+          <p>状态</p>
+          <div class="state_conter">
+            <p>未申报</p>
+            <p>未申报</p>
+            <p>未申报</p>
+            <p>未申报</p>
+            <p>未申报</p>
+            <p>未申报</p>
+          </div>
+        </div>
+        <div class="nature">
+          <p>属性</p>
+          <div class="nature_conter">
+            <p>特大机器</p>
+            <p>特大机器</p>
+            <p>特大机器</p>
+          </div>
+        </div>
+        <div class="source">
+          <p>来源</p>
+          <div class="source_conter">
+            <p>自由</p>
+            <p>自由</p>
+            <p>自由</p>
+          </div>
+        </div>
+        <div class="affair">
+          <button>重置</button>
+          <button>默认</button>
+        </div>
+      </van-popup>
+      <button type="primary" class="dzzy" @click="dzzy">吊装作业</button>
     </main>
   </div>
 </template>
@@ -40,30 +55,32 @@
 <script>
 export default {
   name: "Situ",
+  data() {
+    return {
+      show: false,
+    };
+  },
   methods: {
     add() {
       this.$router.push({
         name: "AddFather",
       });
     },
-<<<<<<< HEAD
-          back(){
-      this.$router.go(-1)
-  },
-  //张浩李涛按钮
-  zhLt(){
-
-  }
-=======
     back() {
       this.$router.go(-1);
     },
-    dzzy(){
+    //张浩李涛按钮
+    zhLt() {
+      this.show = true;
+    },
+    back() {
+      this.$router.go(-1);
+    },
+    dzzy() {
       this.$router.push({
-        path:'HoistingOperation'
-      })
-    }
->>>>>>> 20a47a4d195cc3673b1331a32a1594cbb93cad05
+        path: "HoistingOperation",
+      });
+    },
   },
 };
 </script>
@@ -93,6 +110,119 @@ export default {
     .el-icon-arrow-left,
     .el-icon-bell {
       font-size: 21px;
+    }
+  }
+}
+.van-popup--right {
+  width: 200px;
+  .state {
+    width: 100%;
+    height: 100px;
+    padding: 5px;
+    & > p {
+      font-size: 15px;
+      color: #000;
+      font-weight: 500;
+      width: 100%;
+      height: 40px;
+      line-height: 40px;
+    }
+    .state_conter {
+      width: 100%;
+      height: 60px;
+      padding: 10px;
+      display: flex;
+      flex-wrap: wrap;
+      & > p {
+        margin-right: 5px;
+        width: 50px;
+        height: 20px;
+        background: #ccc;
+        text-align: center;
+        line-height: 20px;
+        border-radius: 15px;
+        margin-bottom: 5px;
+      }
+    }
+  }
+  .source {
+    width: 100%;
+    height: 60px;
+    padding: 5px;
+    & > p {
+      font-size: 15px;
+      color: #000;
+      font-weight: 500;
+      width: 100%;
+      height: 40px;
+      line-height: 40px;
+    }
+    .source_conter {
+      width: 100%;
+      height: 30px;
+      padding: 10px;
+      display: flex;
+      flex-wrap: wrap;
+      & > p {
+        margin-right: 5px;
+        width: 50px;
+        height: 20px;
+        background: #ccc;
+        text-align: center;
+        line-height: 20px;
+        border-radius: 15px;
+        margin-bottom: 5px;
+      }
+    }
+  }
+  .nature {
+    width: 100%;
+    height: 60px;
+    padding: 5px;
+    & > p {
+      font-size: 15px;
+      color: #000;
+      font-weight: 500;
+      width: 100%;
+      height: 40px;
+      line-height: 40px;
+    }
+    .nature_conter {
+      width: 100%;
+      height: 30px;
+      padding: 10px;
+      display: flex;
+      flex-wrap: wrap;
+      & > p {
+        margin-right: 5px;
+        width: 50px;
+        height: 20px;
+        background: #ccc;
+        text-align: center;
+        line-height: 20px;
+        border-radius: 15px;
+        margin-bottom: 5px;
+      }
+    }
+  }
+  .affair{
+    display: flex;
+    width: 100%;
+    margin-top: 100px;
+    &>button{
+      width: 50%;
+      height: 50px;
+      text-align: center;
+      line-height: 50px;
+      border: 0;
+      &:nth-child(1){
+        background: #f6f6f6;
+        color: #6f6f6f;
+      }
+       &:nth-child(2){
+        background: #2a87ff;
+        color: #fff;
+      }
     }
   }
 }
